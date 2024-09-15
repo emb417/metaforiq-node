7edb2d2e2d4724c465357926af0dd0e427c13433
Eric Brousseau <emb417@gmail.com>
2024-09-15 16:54:28 -0700

    separated configs from server

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import { JSONFilePreset } from "lowdb/node";
 import pino from "pino";
 import cron from "node-cron";
 import sendDiscordNotification from "./discordHandler.js";
+import { locations, availableConfig, onOrderConfig, availabilityUrl } from "./configs.js";
 
 const app = express();
 const port = 8008;
@@ -43,32 +44,6 @@ if (!fs.existsSync(dbPath)) {
 
 const db = await JSONFilePreset("db.json", {});
 await db.read();
-
-// Scraper utils
-const locations = [
-  // { code: 9, name: 'Beaverton City Library' },
-  { code: 29, name: "Tigard Public Library" },
-  { code: 31, name: "Tualatin Public Library" },
-  { code: 39, name: "Beaverton Murray Scholls" },
-];
-
-const availableConfig = {
-  type: "available now",
-  fetchUrl:
-    "https://wccls.bibliocommons.com/v2/search?custom_edit=false&query=anywhere%3A(%5B0%20TO%20180%5D)%20%20%20avlocation%3A%22%5B*%20TO%20*%5D%22%20collection%3A%22Best%20Sellers%22%20formatcode%3A(BLURAY%20)&searchType=bl&suppress=true&f_STATUS=39%7C29%7C31&f_NEWLY_ACQUIRED=PAST_180_DAYS&locked=true",
-  scriptValue: 'script[type="application/json"][data-iso-key="_0"]',
-};
-
-const availabilityUrl = (itemId) => {
-  return `https://gateway.bibliocommons.com/v2/libraries/wccls/bibs/${itemId}/availability?locale=en-US`;
-};
-
-const onOrderConfig = {
-  type: "on order",
-  fetchUrl:
-    "https://wccls.bibliocommons.com/v2/search?query=nw%3A%5B0%20TO%20180%5D&searchType=bl&sort=NEWLY_ACQUIRED&suppress=true&title_key=all_newly_acquired&f_FORMAT=BLURAY&f_ON_ORDER=true&f_NEWLY_ACQUIRED=PAST_7_DAYS",
-  scriptValue: 'script[type="application/json"][data-iso-key="_0"]',
-};
 
 function filterItemsByType(type) {
   return db.data.libraryItems.filter((item) => item.type === type);
